ab8d960d5d17aff35e4555f8001d1e29b3f60860
Chris Britt <britt.88@osu.edu>
2017-09-23 22:15:28 -0400

    Finished filecreate

--- filecreate.rb
+++ filecreate.rb
@@ -1,4 +1,6 @@
 require "date"
+require 'fileutils'
+
 
 # Structure:
 # options = {
@@ -29,11 +31,39 @@ EOF
 end
 
 # Structure:
-
+# options = {
+#   filename: *String, # e.g. MyFile.cpp,
+#   directory: *String # e.g. Dir.pwd
+# }
+# return = {
+#   header: String, # The header for the guard
+#   footer: String # The footer for the guard
+# }
 def fileguard_generate(options={})
-<<-EOF
+  raise "fileguard_generate filename must be present" unless options[:filename]
+  raise "fileguard_generate directory must be present" unless options[:directory]
 
+  guard_block = options[:directory]
+    .split(/[\/\\]/)
+    .select{|e| !e.nil? && e.length > 0}
+    .concat(options[:filename]
+      .split("."))
+    .map(&:upcase)
+    .join("_") + "_"
+
+  header = <<-EOF
+#ifndef #{guard_block}
+#define #{guard_block}
 EOF
+
+  footer = <<-EOF
+#endif #{guard_block}
+EOF
+
+  return {
+    header: header,
+    footer: footer
+  }
 end
 
 # Structure:
@@ -44,22 +74,69 @@ end
 #   header: String, # Contents at the top of the file.
 #   body: String, # Contents within the file.
 #   footer: String, # Conents at the end of the file.
-#   fileguard: Boolean, # defaults to: false
+#   fileguard: Boolean, # defaults to: nil,
+#   line_ending: String, # e.g. '\n', defaults to: '\n'
 # }
 def file_compose(options={})
-  options[:directory] = Dir.pwd
-<<-EOF
-#{license_header(options[:license_header])}
+  raise "file_compose filename must be present" unless options[:filename]
 
-#{options[:header]}
+  options[:directory] ||= Dir.pwd
+  options[:license_header] ||= Hash.new
+  options[:line_ending] ||= '\n'
 
-#{options[:body]}
+  fileguard_data = Hash.new
+  if options[:fileguard]
+    fileguard_data = fileguard_generate(options)
+  end
 
-#{options[:footer]}
+  lines = Array.new
 
+  lines.push(license_header(options[:license_header]))
+
+  if fileguard_data[:header]
+    lines.push("")
+    lines.push(fileguard_data[:header])
+  end
+
+  if options[:header]
+    lines.push("")
+    lines.push(options[:header])
+  end
+
+  if options[:body]
+    lines.push("")
+    lines.push(options[:body])
+  end
+
+  if options[:footer]
+    lines.push("")
+    lines.push(options[:footer])
+  end
+
+  if fileguard_data[:footer]
+    lines.push("")
+    lines.push(fileguard_data[:footer])
+  end
+
+  return <<-EOF
+#{lines.join(options[:line_ending])}
 EOF
 end
 
 # Structure:
 # See file_compose
+#
+# This function just wraps around file_compose
 def file_write(options={})
+  raise "file_write filename must be present" unless options[:filename]
+
+  options[:directory] ||= Dir.pwd
+
+  filepath = File.join(options[:directory], options[:filename])
+
+  unless File.directory?(options[:directory])
+    FileUtils.mkdir_p(options[:directory])
+  end
+
+  File.open(filepath, 'w') { |file| file.write(file_compose(options)) }
+end
